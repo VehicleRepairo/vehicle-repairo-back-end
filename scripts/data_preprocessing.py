dbc4170632970e3e48d4d04ffa6a9d1f5953a3bc
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-06 16:52:07 +0530

    Setting up dataset

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,3 +1,9 @@
 #Importing necessary libraries and dependencies
 from sklearn import linear_model
 import pandas as pd
+
+#Setting up dataset path
+dataset_path = "../data/vehicle_maintenance_dataset.csv"
+#Reading the dataset file and storing it in a pandas data frame variable
+df = pd.read_csv(dataset_path)
+print(df.columns)
