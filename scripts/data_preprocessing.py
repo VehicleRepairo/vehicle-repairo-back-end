41f01f86d6f22d1c6b589c11453f01a012a6f37e
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-06 17:09:30 +0530

    One-hot encoded string columns

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -7,3 +7,6 @@ dataset_path = "../data/vehicle_maintenance_dataset.csv"
 #Reading the dataset file and storing it in a pandas data frame variable
 df = pd.read_csv(dataset_path)
 print(df.columns)
+
+#One-Hot Encoding the categorical columns
+df = pd.get_dummies(df, columns=['brand', 'model', 'engine_type'], prefix=None)
