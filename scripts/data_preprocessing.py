d6d0c28bb0edc493c3d705974e66b581de40d9fa
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-07 12:41:31 +0530

    converting service types from int to bool and checking changes

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -9,4 +9,17 @@ df = pd.read_csv(dataset_path)
 
 #One-Hot Encoding the categorical columns
 df = pd.get_dummies(df, columns=['brand', 'model', 'engine_type'], prefix=None)
-print(df)
+
+#Storing service types in a variable
+service_types = ['oil_filter', 'engine_oil', 'washer_plug_drain', 'dust_and_pollen_filter', 'wheel_alignment_and_balancing'
+                ,'air_clean_filter', 'fuel_filter', 'spark_plug', 'brake_fluid', 'brake_and_clutch_oil', 'transmission_fluid',
+                'brake_pads', 'clutch', 'coolant']
+
+#Converting service types from int to bool
+df[service_types] = df[service_types].astype(bool)
+
+#Previewing pre-processed data
+#print(df.columns)
+#print(df.head())
+#print(df.info())
+#print(df.describe())
