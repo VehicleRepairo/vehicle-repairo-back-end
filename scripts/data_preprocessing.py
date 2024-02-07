6de46c657793d2c7accbe447274f4db839ea01af
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-07 23:19:29 +0530

    changed converting method of categorical values from get dummies to one hot encoding

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,14 +1,29 @@
 #Importing necessary libraries and dependencies
-from sklearn import linear_model
+from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
+import numpy as np
 
 #Setting up dataset path
 dataset_path = "../data/vehicle_maintenance_dataset.csv"
 #Reading the dataset file and storing it in a pandas data frame variable
 df = pd.read_csv(dataset_path)
 
-#One-Hot Encoding the categorical columns
-df = pd.get_dummies(df, columns=['brand', 'model', 'engine_type'], prefix=None)
+#Finding out unique values in brand, model and engine type
+#print(df["brand"].unique())
+#print(df["model"].unique())
+#print(df["engine_type"].unique())
+
+#Object containing one-hot encoder
+ohe = OneHotEncoder()
+
+#Storing one-hot encoded categorical values
+category_features = ohe.fit_transform(df[["brand", "model", "engine_type"]]).toarray()
+
+#Combining converted arrays of one-hot encoded values to one
+np.array(category_features).ravel()
+
+#Storing the one-hot coded changes in the dataframe
+pd.DataFrame(category_features)
 
 #Storing service types in a variable
 service_types = ['oil_filter', 'engine_oil', 'washer_plug_drain', 'dust_and_pollen_filter', 'wheel_alignment_and_balancing'
@@ -20,6 +35,6 @@ df[service_types] = df[service_types].astype(bool)
 
 #Previewing pre-processed data
 #print(df.columns)
-#print(df.head())
+print(df.head())
 #print(df.info())
-#print(df.describe())
+#print(df.describe()) 
