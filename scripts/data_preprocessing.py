00a192413da9a82ee612aa8338e5e2b7385c05c6
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-08 12:05:43 +0530

    converted encoded columns from float to bool

--- scripts/data_preprocessing.py
+++ scripts/data_preprocessing.py
@@ -1,5 +1,7 @@
 #Importing necessary libraries and dependencies
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 import pandas as pd
 import numpy as np
 
@@ -31,11 +33,18 @@ service_types = ['oil_filter', 'engine_oil', 'washer_plug_drain', 'dust_and_poll
                 ,'air_clean_filter', 'fuel_filter', 'spark_plug', 'brake_fluid', 'brake_and_clutch_oil', 'transmission_fluid',
                 'brake_pads', 'clutch', 'coolant']
 
+#Storing one-hot encoded columns in a variabel
+encoded_columns = ['brand_honda', 'brand_toyota', 'model_amaze', 'model_city', 'model_fortuner', 'model_jazz', 'engine_type_diesel'           
+                ,'engine_type_petrol']
+
 #Converting service types from int to bool
 df_encoded[service_types] = df[service_types].astype(bool)
 
+#Converting encoded columns from float to bool
+df_encoded[encoded_columns] = df_encoded[encoded_columns].astype(bool)
+
 #Previewing pre-processed data
 #print(df_encoded.columns)
-#print(df_encoded.head())
+print(df_encoded.head())
 print(df_encoded.info())
 #print(df.describe()) 
