814bd91925f64eda82e8d5f3e6ccea231f8132c1
Nanthushan <130738022+Nanthushan2004@users.noreply.github.com>
2024-02-10 19:44:22 +0530

    commit

--- scripts/training_testing_data.py
+++ scripts/training_testing_data.py
@@ -1,3 +1,10 @@
 #Importing necessary libraries and dependencies
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+import pandas as pd
+
+#Setting up pre-processed dataset path
+dataset_path = "../data/preprocessed_vehicle_maintenance_dataset.csv"
+
+#Storing pre-processed data in a data frame
+df = pd.read_csv(dataset_path)
