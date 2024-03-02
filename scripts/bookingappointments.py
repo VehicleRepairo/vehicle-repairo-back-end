aa92584d7ea8bb9944943b74ef2604a4989896dc
hirushi24 <hirushi.20220615@iit.ac.lk>
2024-03-02 16:15:06 +0530

    updated create appointments

--- scripts/bookingappointments.py
+++ scripts/bookingappointments.py
@@ -1,8 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from pymongo import MongoClient
 from bson import ObjectId
-#from dotenv import load_dotenv, find_dotenv
 import json
 from flask_cors import CORS
 
@@ -10,43 +9,27 @@ app = Flask(__name__)
 api = Api(app)
 CORS(app)
 
-#Setting up the MongoDB connection
+# Setting up the MongoDB connection
 connection_string = "mongodb://localhost:27017"
 client = MongoClient(connection_string)
 
-db = client.VehicleRepairoDb
-
-appointments_collection = db.appointments
-
-def create_appointment():
-    user = ""
-    vehicle_brand = ""
-    vehicle = ""
-    Date_of_appointment = ""
-    Appointment_time = ""
-    Service_Required = ""
-    
-    #for user, vehicle_brand, vehicle_model, Date_of_appointment, Appointment_time, Service_required in :
-     #   appointments_collection.insert_one()
-
-
-create_appointment()
-
-
+# Specify the database
 db = client['VehicleRepairo']
 
 # Define collections
 appointments_collection = db['appointments']
 
+
 class AllAppointmentsResource(Resource):
     def get(self):
         appointments = appointments_collection.find()
-        if appointments:
+        if appointments.count() > 0:
             return [json.loads(json.dumps(appointment, default=str)) for appointment in appointments], 200
         else:
             return {'message': 'No appointments found'}, 404
 
-api.add_resource(AllAppointmentsResource, '/appointment') 
+api.add_resource(AllAppointmentsResource, '/appointment')
+
 
 class AppointmentResource(Resource):
     def get(self, appointment_id):
@@ -55,13 +38,6 @@ class AppointmentResource(Resource):
             return json.loads(json.dumps(appointment, default=str)), 200
         else:
             return {'message': 'Appointment not found'}, 404
-
-    def post(self):
-        data = request.get_json()
-        appointment_id = create_appointment(data.get('user'), data.get('vehicle_brand'), data.get('vehicle'), 
-                                             data.get('Date_of_appointment'), data.get('Appointment_time'), 
-                                             data.get('Service_Required'))
-        return {'appointment_id': appointment_id}, 201
 
     def put(self, appointment_id):
         data = request.get_json()
@@ -77,8 +53,40 @@ class AppointmentResource(Resource):
             return {'message': 'Appointment deleted successfully'}, 200
         else:
             return {'message': 'Appointment not found'}, 404
-        
-api.add_resource(AppointmentResource, '/appointment')
+
+
+@app.route('/create_appointment', methods=['POST'])
+def create_appointment():
+    data = request.json
+    user_uid = data.get('user_uid')
+
+    # Check if user_uid exists in the vehicle collection
+    vehicle_data = db.vehicle.find_one({'user_uid': user_uid})
+    if not vehicle_data:
+        return jsonify({'error': 'User not found in the vehicle database'})
+
+    # Extract vehicle_brand and vehicle from vehicle_data
+    vehicle_brand = vehicle_data.get('vehicle_brand')
+    vehicle = vehicle_data.get('vehicle')
+
+    # Create appointment
+    appointment_data = {
+        'Appointments_time': data.get('Appointments_time'),
+        'Date_of_appointment': data.get('Date_of_appointment'),
+        'user_uid': user_uid,
+        'Service_Required': data.get('Service_Required'),
+        'vehicle_brand': vehicle_brand,
+        'vehicle': vehicle,
+        'Mech_uid': data.get('Mech_uid')
+    }
+
+    # Insert appointment into the appointment collection
+    appointment_id = db.appointment.insert_one(appointment_data).inserted_id
+
+    return jsonify({'success': True, 'appointment_id': str(appointment_id)})
+
+
+api.add_resource(AppointmentResource, '/appointment/<string:appointment_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
