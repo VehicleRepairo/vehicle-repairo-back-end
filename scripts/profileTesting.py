97e27258a2acce84f50ee10725b789713ced757b
Shiwarne Silva <155060453+SenadiIlleperuma@users.noreply.github.com>
2024-03-03 18:24:16 +0530

    all the average ratings of the mechanics is shown when the program runs

--- scripts/profileTesting.py
+++ scripts/profileTesting.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://senadi20220678:%23abc%40123@vehicle-repairo.dlhnhh6.mongodb.net/")
 db = client['Ratings_and_Reviews']
-# collection = db['ratings']
 mechanics_collection = db['mechanics']
 
 # Function to get mechanic's average rating
@@ -15,11 +14,11 @@ def get_mechanic_average_rating(mechanic_id):
         return None
 
 if __name__ == "__main__":
-    mechanic_id = input("Enter the Mechanic ID : ")
-    average_rating = get_mechanic_average_rating(mechanic_id)
-    
-    if average_rating is not None:
-        print("Mechanic ID:", mechanic_id)
-        print("Average Rating:", average_rating)
-    else:
-        print("Mechanic not found.")
+    mechanics = mechanics_collection.find()
+    for mechanic in mechanics:
+        mechanic_id = mechanic.get('mechanic_id')
+        average_rating = get_mechanic_average_rating(mechanic_id)
+        if average_rating is not None:
+            print(f"Mechanic ID {mechanic_id} - Average rating ({average_rating})")
+        else:
+            print(f"Mechanic ID {mechanic_id} - Average rating (Not available)")
