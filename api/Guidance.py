1b6206920467e780ebcc20bc482561d305ebfd57
radildamsa <158327147+radildamsa@users.noreply.github.com>
2024-03-08 11:18:01 +0530

    Network connection error resolved

--- api/Guidance.py
+++ api/Guidance.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pymongo
+from bson import ObjectId
 from rake_nltk import Rake
 import nltk
-from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
 def find_word_in_table(collection, target_word):
     matching_rows = []
     rake = Rake()  # Initialize RAKE
@@ -18,13 +20,11 @@ def find_word_in_table(collection, target_word):
 
         # Check if target word is in any of the keywords
         if any(target_word.lower() in keyword.lower() for keyword in keywords):
+            # Convert ObjectId to string
+            document['_id'] = str(document['_id'])
             matching_rows.append(document)
 
     return matching_rows
-
-@app.route('/')
-def index():
-    return 'Welcome to my Flask application!'
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -43,15 +43,7 @@ def search():
     }
     return jsonify(response)
 
-# CORS handling
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
-
 if __name__ == "__main__":
     nltk.download('stopwords')
     nltk.download('punkt')
-    app.run(debug=True)  # Run the Flask app
+    app.run(host='0.0.0.0', port=8000)
